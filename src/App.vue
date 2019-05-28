28c44db594ae17cf0c2d16c1a991cb02211fd691
faifairy <faifairy@126.com>
2019-05-29 00:20:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="todo" @keydown="add($event)">
+
+    <h2>进行中</h2>
+
+    <ul>
+      <li v-for="(item,k) in list" v-if="!item.checked">
+          <input type="checkbox" v-model="item.checked" @change="saveList()">{{item.title}} <button @click="remove(k)">删除</button>
+      </li>
+    </ul>
+
+    <h2>已完成</h2>
+
+    <ul>
+      <li v-for="(item,k) in list" v-if="item.checked">
+          <input type="checkbox" v-model="item.checked" @change="saveList()">{{item.title}} <button @click="remove(k)">删除</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      todo: '',
+      list: []
+    }
+  },
+  methods: {
+      add(e){
+        if (e.keyCode==13) {
+            this.list.push({
+              title: this.todo,
+              checked: false
+            });
+            
+        localStorage.setItem('list',JSON.stringify(this.list));
+        
+        }
+      },
+      remove(k){
+        this.list.splice(k,1);
+
+        localStorage.setItem('list',JSON.stringify(this.list));
+        
+      },
+      saveList(){
+        localStorage.setItem('list',JSON.stringify(this.list));
+        
+      } 
+  },
+  mounted(){
+        var list=JSON.parse(localStorage.getItem('list'));
+        
+        if (list) {
+          this.list=list;
+        }
+      }
+}
+</script>
+
+<style>
+  .purple {
+    color: purple;
+  }
+
+  .pink {
+    color: pink;
+  }
+
+  .fuchsia {
+    color: fuchsia;
+  }
+
+  .box {
+    width: 300px;
+    height: 100px;
+    border: 1px solid #ccc;
+  }
+</style>
